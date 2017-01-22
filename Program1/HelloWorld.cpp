e337921da2d61ab8f3d1c63e711aa077c0f84d13
Jordan <jordan.dwo@gmail.com>
2017-01-22 12:20:00 -0700

    A description of Myself
    An output of strings decribing myself for the teacher.

---
+++ Program1/HelloWorld.cpp
@@ -0,0 +1,30 @@
+/* Jordan Dworaczyk
+ * CSC240AA - Program #1 - C++
+ * Due Monday, January 23
+ * Warm-up exercise to become familiar with a programming enviroment and the
+ * process of preparing and delivering a program.
+ */
+
+ #include <iostream>
+ using namespace std;
+
+ int main()
+ {
+   cout << "My name is Jordan Dworaczyk." << endl;
+   cout << "I have taken Principles of Programming in Java, Digital Design"<< endl;
+   cout << "Fundamentals, and Object Oriented Programming and Data Structres.\n";
+   cout << "I sometimes use a computer for Excel when I am at work, but besides\n";
+   cout << "this I have no professional background with computers.\n";
+   cout << "I will most likely use a Lenovo computer with Windows 8.1 or a HP\n";
+   cout << "Pavalloin with Ubuntu for my projects.\n";
+   cout << "I have used Java in the past.\n";
+   cout << "My personal goal is to learn as much about the material as possible\n";
+   cout << "as well as software developement and computerscience.\n";
+   cout << "I am most concerned about time management.\n";
+   cout << "I am currently taking Calculus I, Introduction to Logic, \n";
+   cout << "Engineering Analyss Tools and Techniques, as well as this course.\n";
+   cout << "The program that I liked the best was a program that solves a \n";
+   cout << "three demensional maze recursively.\n";
+   cout << "I play guitar and bass as a hobby.\n";
+   return 0;
+ }
