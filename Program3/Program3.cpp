15e821c85634089ce2a4cd02da0738a132155770
Jordan Dworaczyk <jordan.dwo@gmail.com>
2017-02-07 23:54:57 -0700

    Did a lot of stuff
    lest just be honest here. its a shit fuck of commits

--- Program3/Program3.cpp
+++ Program3/Program3.cpp
@@ -8,10 +8,17 @@
 #include <iostream>
 using namespace std;
 #include <math.h>
+#include <stdio.h>
 
 void breifIntroForUser(void);
 void collectUserInput(double &charge, double &threshold, double &init_balance,
                       double &rate, int &period, int &choice);
+double collectCharge(void);
+double collectThreshold(void);
+double collectInitBalance(void);
+double collectRate(void);
+int collectPeriod(void);
+int collectChoice(int period);
 double validateNonNegative(double user_input);
 int validatePeriod(int user_input);
 void printPeriod(int period);
@@ -21,27 +28,136 @@ void accountSummary(double init_balance, double rate, double charge,\
     double threshold,
     int period,
     int choice);
+int requestTable(void);
 double getFinalAccountBalance(double init_balance, double rate, double charge,
-    double threshold);
+    double threshold, int is_table, int length_of_investment);
 int getLengthToReachGoal(double init_balance, double rate, double charge,
     double threshold,
-    int period,
+    int period, int is_table,
     double &final_balance);
 
 
 int main() {
 
-  double init_balance, rate, charge, threshold;
-  int period, choice;    breifIntroForUser();
+  double init_balance, rate, charge, threshold, final_balance, amount;
+  int period, choice, is_table, length_of_investment;
+  breifIntroForUser();
+
+  int user_input;
   collectUserInput(charge, threshold, init_balance, rate, period, choice);
-  accountSummary(init_balance, rate, charge, threshold, period, choice);
+  //accountSummary(init_balance, rate, charge, threshold, period, choice);
 
   if(choice == 0){
-    getFinalAccountBalance(init_balance, rate, charge, threshold);
+
+    cout << endl << "Please, specify the length of the investment." << endl;
+    cout << "Enter the amount of ";
+    printPeriod(period);
+    cout << ":";
+    cin  >> length_of_investment;
+    cout << endl;
+    
+    is_table = requestTable();
+
+    final_balance =
+        getFinalAccountBalance(init_balance, rate, charge, threshold,
+        is_table, length_of_investment);
+    cout << "The final balance of your account is $" << final_balance
+         << " after " << length_of_investment << " ";
+    printPeriod(period);
+    cout << "." << endl;
   } else {
-    getLengthToReachGoal(init_balance, rate, charge, threshold, period,
-        final balance);
+
+    cout << endl << "Please, specify the amount you would like to achieve.\n"
+         << "Enter the amount: $";
+    cin  >> amount;
+    amount = validateNonNegative(amount);
+    cout << endl;
+
+    is_table = requestTable();
+
+
+    length_of_investment =
+        getLengthToReachGoal(init_balance, rate, charge, threshold, period,
+        is_table, amount);
+
+    cout << "You reached your goal afer " << length_of_investment << " ";
+    printPeriod(period);
+    cout << " with a final total balance of \n$" << amount << endl;
   }
+
+  do {
+
+  cout << endl << "Would you like to do another calculation (with the same \n"
+       << "processing charge and threshold)?\n" << "Please, enter your choice "
+       << "(0 for no & 1 for yes): ";
+  cin  >> user_input;
+  user_input = validateChoice(user_input);
+  cout << endl;
+
+  if(user_input == 1) {
+    init_balance = collectInitBalance();
+    rate = collectRate();
+    period = collectPeriod();
+    choice = collectChoice(period);
+    is_table = requestTable();
+
+    if(choice == 0) {
+      final_balance =
+          getFinalAccountBalance(init_balance, rate, charge, threshold,
+          is_table, length_of_investment);
+      cout << "\nThe final balance of your account is $" << final_balance
+           << " after " << length_of_investment << " ";
+      printPeriod(period);
+      cout << "." << endl << endl;
+
+  } else {
+
+      length_of_investment =
+          getLengthToReachGoal(init_balance, rate, charge, threshold, period,
+          is_table, amount);
+
+      cout << "\nYou reached your goal afer " << length_of_investment << " ";
+      printPeriod(period);
+      cout << " with a final total balance of \n$" << amount << endl << endl;
+    }
+
+  } else {
+
+    cout << "Would you rather try a different processing charge and threshold?"
+     << endl << "Enter 0 for yes & 1 for no: ";
+    cin  >> user_input;
+    user_input = validateChoice(user_input);
+    cout << endl;
+
+    if(user_input == 1) {
+      charge = collectCharge();
+      threshold = collectThreshold();
+      is_table = requestTable();
+
+      if(choice == 0) {
+        final_balance =
+            getFinalAccountBalance(init_balance, rate, charge, threshold,
+            is_table, length_of_investment);
+        cout << "\nThe final balance of your account is $" << final_balance
+             << " after " << length_of_investment << " ";
+        printPeriod(period);
+        cout << "." << endl << endl;
+
+      } else {
+
+        length_of_investment =
+            getLengthToReachGoal(init_balance, rate, charge, threshold, period,
+            is_table, amount);
+
+        cout << "\nYou reached your goal afer " << length_of_investment << " ";
+        printPeriod(period);
+        cout << " with a final total balance of \n$" << amount << endl << endl;
+      }
+    }
+  }
+} while(user_input != 0);
+
+  cout << "Thats all folks!" << endl;
   return 0;
  }
 
@@ -53,30 +169,51 @@ int main() {
 void collectUserInput(double &charge, double &threshold, double &init_balance,
                       double &rate, int &period, int &choice) {
 
+  charge = collectCharge();
+  threshold = collectThreshold();
+  init_balance = collectInitBalance();
+  rate = collectRate();
+  period = collectPeriod();
+  choice = collectChoice(period);
+}
 
+double collectCharge(void){
+  double charge;
   cout << "Please specify the bank's annual processing charge and threshold "
        << "in dollars." << '\n';
   cout <<  "Enter Annual Processing Charge: $";
   cin  >> charge;
   charge = validateNonNegative(charge);
-
+  return charge;
+}
+double collectThreshold(void) {
+  double threshold;
   cout << "Enter Threshold: $";
   cin  >> threshold;
   threshold = validateNonNegative(threshold);
-
+  return threshold;
+}
+double collectInitBalance(void){
+  double init_balance;
   cout << endl;
   cout << "Please specify the initial account balance in dollars." << endl;
   cout << "Enter initial account ballance: $";
   cin  >> init_balance;
   cout << endl;
   init_balance = validateNonNegative(init_balance);
-
+  return init_balance;
+}
+double collectRate(void){
+  double rate;
   cout << "Please enter the annual interest rate." << endl;
   cout << "Enter annual interest rate: ";
   cin  >> rate;
   cout << endl;
   rate = validateNonNegative(rate);
-
+  return rate;
+}
+int collectPeriod(void){
+  int period;
   cout << "Please enter the period at which the interest is compounded." <<endl;
   cout << "If daily, then enter a zero  (i.e 0)." << endl;
   cout << "If monthly, then enter a one (i.e 1)." << endl;
@@ -85,7 +222,11 @@ void collectUserInput(double &charge, double &threshold, double &init_balance,
   cin  >> period;
   cout << endl;
   period = validatePeriod(period);
+  return period;
 
+}
+int collectChoice(int period){
+  int choice;
   cout << endl;
   cout << "Please select one of the two options:" << endl;
   cout << "(A) Enter the length in ";
@@ -98,12 +239,13 @@ void collectUserInput(double &charge, double &threshold, double &init_balance,
   cout << "(B) Specify a goal for the account balance, and have the" << endl;
   cout << "program calculate how long in ";
   printPeriod(period);
-  cout << " it will take to achieve that amount.";
+  cout << " it will take to achieve that ammount.";
   cout << endl;
   cout << endl << "Please enter 0 for A, or enter 1 for B." << endl;
   cout << "Enter choice (0 or 1): ";
   cin  >> choice;
-  choice = validateChoice(choice);;
+  choice = validateChoice(choice);
+  return choice;
 }
 
 double validateNonNegative(double user_input) {
@@ -124,7 +266,7 @@ int validatePeriod(int user_input) {
     cout << "Enter period: ";
     cin  >> user_input;
   }
-  return 0;
+  return user_input;
 }
 
 void printPeriod(int period) {
@@ -176,16 +318,96 @@ void accountSummary(double init_balance, double rate, double charge,\
   cout << endl;
 }
 
-double getFinalAccountBalance(double init_balance, double rate, double charge,
-    double threshold) {
+int requestTable() {
+  int user_input;
+  cout << "\nWould you like a table of each decribing the account balance "
+       << "at the end of each period?\n" << "Enter 0 for no and 1 for yes: ";
+  cin  >> user_input;
+  user_input = validateChoice(user_input);
+  return user_input;
+}
 
-  return 0;
+double getFinalAccountBalance(double init_balance, double rate, double charge,
+    double threshold, int is_table, int length_of_investment ) {
+  //for debugging
+  /*cout << endl <<  "init_balance: " << init_balance << endl;
+  cout << "rate: " << rate << endl;
+  cout << "charge: " << charge << endl;
+  cout << "threshold: " << threshold << endl;
+  cout << "is_table: " << is_table << endl;
+  cout << "length_of_investment: " << length_of_investment << endl << endl;*/
+
+  int i;
+  double balance = init_balance;
+  double interest, temp_balance;
+  for(i = 0; i < length_of_investment; i++) {
+
+    if(balance < threshold) {
+      balance = balance - charge;
+      //cout << endl << "balance after penalty: " << balance << endl; //debugging
+    }
+    interest = balance * (rate / 100);
+    //cout << endl << "interest: " << interest << endl; //debugging
+    temp_balance = balance + interest;
+    //cout << "new balance: " << temp_balance << endl; //debugging
+    balance = temp_balance;
+    //cout << endl << "balance: " << balance << endl; //debugging
+
+    if(is_table == 1) {
+      printf("%*.2f ", 3 , balance);
+
+      int n;
+      for(n = 0; n < i; n++) {
+        cout << "*";
+      }
+    cout << endl;
+    }
+
+  }
+  cout << endl << endl;
+
+  return balance;
 }
 
 int getLengthToReachGoal(double init_balance, double rate, double charge,
     double threshold,
-    int period,
-    double &final_balance){
+    int period, int is_table,
+    double &amount) {
 
+  if(is_table == 1) {
+    is_table = 2;
+  }
+  int length_of_investment = 1;
+  while(length_of_investment) {
+    double balance =
+        getFinalAccountBalance(init_balance, rate, charge, threshold,
+        is_table,length_of_investment);
+    //cout << "balance: " << balance << endl; //debugging
+
+    if(balance >= amount) {
+      amount = balance;
+      if(is_table == 2) {
+        is_table = 1;
+      }
+      return length_of_investment;
+    } else {
+      length_of_investment++;
+    }
+
+    if(is_table == 2) {
+      printf("%*.2f ", 3 , balance);
+
+      int n;
+      for(n = 1; n < length_of_investment; n++) {
+        cout << "*";
+      }
+    cout << endl;
+    }
+
+  }
+
+  if(is_table == 2) {
+    is_table = 1;
+  }
   return 0;
 }
