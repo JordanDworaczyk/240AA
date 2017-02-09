4eae90074ebb20456669b9fca4d232e75d86f0e3
Jordan Dworaczyk <jordan.dwo@gmail.com>
2017-02-08 20:20:26 -0700

    Fixed stupid mouse slips

--- HW/HW3/Homework#3.cpp
+++ HW/HW3/Homework#3.cpp
@@ -5,20 +5,22 @@
 * Due Wed. Feb. 8th
 * Write a program in C++ to manipulate arrays.
 */
+#include <iostream>
+using namespace std;
 
 int askUserArraySize(void);
 
 int main() {
   int array_size;
-  array_size = askUserArraySize(void);
-  cout <<arraysize;
+  array_size = askUserArraySize();
+
   return 0;
 }
 
 int askUserArraySize(void) {
   int user_input;
 
-  cout <<"How big do you want the array to be?\nEnter array size: "<<endl;
+  cout <<"How big do you want the array to be?\nEnter array size: ";
   cin  >> user_input;
   cout << endl;
 
