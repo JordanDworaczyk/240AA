caa57adc1f0a3b38d0e332830b2c633954874a01
Jordan Dworaczyk <jordan.dwo@gmail.com>
2017-02-08 20:53:20 -0700

    Finished Problem 1
    Write a subprogram that asks the user for the size of a list of integers, then allocates an array of that size, and then repeatedly asks the user for each number and stores it in the next element of the array. Return the pointer to the new array to the calling function (using the 'return' statement).

--- HW/HW3/Homework#3.cpp
+++ HW/HW3/Homework#3.cpp
@@ -8,21 +8,51 @@
 #include <iostream>
 using namespace std;
 
-int askUserArraySize(void);
+int *collectUserArray(void);
+int *findMatchingIntegers(int list_given[], int list_size, int target);
 
 int main() {
-  int array_size;
-  array_size = askUserArraySize();
+  int *user_array;
+  user_array = collectUserArray();
+
+
 
   return 0;
 }
 
-int askUserArraySize(void) {
-  int user_input;
+int *collectUserArray(void) {
+  int user_input, array_length;
+  int *list_integers;
 
-  cout <<"How big do you want the array to be?\nEnter array size: ";
-  cin  >> user_input;
+  cout <<"How big do you want the array to be?\n";
+  do {
+    cout << "Enter array size: ";
+    cin  >> user_input;
+    cout << endl;
+  } while(user_input < 0);
+
+  array_length = user_input;
+  list_integers = new int[array_length];
+
+  cout << "Please, enter your integers into the array.\n";
+  for(int i = 0; i < array_length; i++) {
+    cout << "Enter Integers: ";
+    cin  >> user_input;
+
+    list_integers[i] = user_input;
+  }
   cout << endl;
-
-  return user_input;
+  return list_integers;
 }
+
+/*
+int *findMatchingIntegers(int list_given[], int list_size,  int target) {
+  int *matches;
+  matches = new int[list_size];
+
+  for(int i = 0; i < list_size; i++ ) {
+
+  }
+
+  return 0;
+}*/
