93bea6b8541c07c12f18c422b55f91b1c180e18e
Jordan <jordan.dwo@gmail.com>
2017-04-09 23:29:30 -0700

    Got the little matrix to work

--- Program12A.rkt/Program#12A.rkt
+++ Program12A.rkt/Program#12A.rkt
@@ -10,6 +10,13 @@
 
 ; utility functions
 
+(define (myAbsolute value)
+  (if (< value 0)
+    (* value -1)
+    value
+  )
+)
+
 (define (myListLength Lst)
   (if (null? Lst)
     0
@@ -19,7 +26,7 @@
 
 (define (myGetElement Lst Pos)
   (if (null? Lst)
-    0
+    ()
     (if (= Pos 1)
       (car Lst)
       (myGetElement (cdr Lst)(- Pos 1))
@@ -35,6 +42,64 @@
   (myGetElement Lst Column )
 )
 
+(define (myFindElement Lst Pos)
+  (if (null? Lst)
+    ()
+    (if (= Pos 1)
+      Lst
+      (myFindElement (cdr Lst)(- Pos 1))
+    )
+  )
+)
+
+(define (myFindRow Lst Row)
+  (myFindElement Lst Row)
+)
+
+(define (myFindColumn Lst Column)
+  (myFindElement Lst Column )
+)
+
+; insert at given position (assuming positions start at 1)
+
+(define (myInsertAtPos Lst Item Pos)
+  (if (= Pos 1)
+    (cons Item Lst)
+    (if (null? Lst)
+      ()
+      (cons (car Lst) (myInsertAtPos (cdr Lst) Item (- Pos 1)))
+    )
+  )
+)
+
+; delete the front
+(define (myDeleteFront Lst)
+  (cdr Lst)
+)
+
+; delete the end
+(define (myDeleteEnd Lst)
+  (if (null? Lst)
+    ()
+    (myDeleteEndX Lst)
+  )
+)
+
+; delete the end (assuming non-empty list)
+(define (myDeleteEndX Lst)
+  (if (null? (cdr Lst))
+    ()
+    (cons (car Lst) (myDeleteEndX (cdr Lst)))
+  )
+)
+
+; delete end x times
+(define (deleteEndXTimes lst x times)
+  (if (= x times)
+    lst
+    (deleteEndXTimes (myDeleteEnd lst) x (+ times 1))
+  )
+)
 (Matrix)
 
 
@@ -42,6 +107,11 @@
 
 (define (getCell Matrix Row Column)
   (getColumn (getRow Matrix Row) Column)
+)
+
+(define (firstPart lst Row Column)
+  (deleteEndXTimes (getRow lst Row) (- (myListLength
+    (getRow lst Row)) (- Column 1)) 0)
 )
 
 (getCell (Matrix) 1 1)
@@ -60,3 +130,95 @@
 
 ; setCell: Returns a new matrix with the given grid location reset to the given
 ; value 'Item'
+
+(define (middle Lst Row Column Item)
+  (myInsertAtPos
+    (myFindElement
+      (myInsertAtPos
+        (getRow Lst Row)
+        Item
+        (+ Column 1)
+      )
+      (+ 1 Column)
+    )
+    (firstPart Lst Row Column)
+    1
+  )
+)
+
+(define (combine lstA lstB)
+  (if (null? (cdr lstA))
+    (cons
+      (car lstA)
+      lstB
+    )
+    (cons
+      (car lstA)
+      (combine (cdr lstA) lstB)
+    )
+  )
+)
+
+(define (insertLittle Matrix Row Column Item)
+  (if (null? Matrix)
+    ()
+    (if (= 1 Column)
+      (myInsertAtPos
+        (cdr (getRow Matrix Row))
+        Item
+        Column
+      )
+      (if (= Column (myListLength (getRow Matrix Row)))
+        (myDeleteEnd
+          (myInsertAtPos
+            (getRow Matrix Row)
+            Item
+            Column
+          )
+        )
+        (combine
+          (firstPart Matrix Row Column)
+          (cdr (middle Matrix Row Column Item))
+        )
+      )
+    )
+  )
+
+)
+
+(define (setCell Matrix Row Column Item )
+  (if (= 1 Column)
+    (myInsertAtPos
+      (cdr Matrix)
+      (insertLittle Matrix Row Column Item)
+      Row
+    )
+    (if (= Column (myListLength Matrix))
+      (myDeleteEnd
+        (myInsertAtPos
+          (getRow Matrix Row)
+          (insertLittle Matrix Row Column Item)
+          Column
+        )
+      )
+      (combine
+        (firstPart Matrix Row Column)
+        (cdr (middle Matrix Row Column (insertLittle Matrix Row Column Item)))
+      )
+    )
+  )
+)
+
+
+(setCell (Matrix) 1 1 10)
+(setCell (Matrix) 1 2 10)
+(setCell (Matrix) 1 3 10)
+(setCell (Matrix) 1 4 10)
+(setCell (Matrix) 2 1 10)
+(setCell (Matrix) 2 2 10)
+(setCell (Matrix) 2 3 10)
+(setCell (Matrix) 2 4 10)
+(setCell (Matrix) 3 1 10)
+(setCell (Matrix) 3 2 10)
+(setCell (Matrix) 3 3 10)
+(setCell (Matrix) 3 4 10)
